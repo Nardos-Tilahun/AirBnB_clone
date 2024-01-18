0eb5056e05effa156bfad50bc99f4f9df5f7f2eb
Nardos-Tilahun <contactnardos@gmail.com>
2024-01-18 15:23:33 +0300

    airbnb clone task-3

--- models/base_model.py
+++ models/base_model.py
@@ -10,32 +10,30 @@ class BaseModel:
     """
     The BaseModel class that define common properties of the classes
     """
-    
+
     def __init__(self, *args, **kwargs):
         """
         This method initiate the object as a constractor
         """
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        
     def save(self):
         """
             used to save the the object at the current date time
         """
-        self.updated_at = datetime.now();
-    
+        self.updated_at = datetime.now()
+
     def to_dict(self):
         """
         return dictionary of the instances
         """
-        inst_dict = self.__dict__.copy();
-        inst_dict["__class__"] = self.__class__.__name__;
-        inst_dict["created_at"] = self.created_at.isoformat();
-        inst_dict["updated_at"] = self.updated_at.isoformat();
-        
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class__.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
         return inst_dict
 
     def __str__(self):
@@ -43,7 +41,8 @@ class BaseModel:
         """
         class_name = self.__class__.__name__
         return f"[{class_name}] ({self.id}) {self.__dict__}"
-            
+
+
 if __name__ == '__main__':
     my_model = BaseModel()
     my_model.name = "My First Model"
@@ -55,4 +54,5 @@ if __name__ == '__main__':
     print(my_model_json)
     print("JSON of my_model:")
     for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+        print("\t{}: ({}) - {}".format(
+            key, type(my_model_json[key]), my_model_json[key]))
